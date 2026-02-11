d95c0c1514e75886294dce6b0191800cb3ae2276
Yuuki Nakamura <yuppyi777@gmail.com>
2026-02-11 22:46:16 +0900

    fix: Pydantic v2の設定を修正して未定義の環境変数を無視するように変更

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -17,9 +17,11 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    class Config:
-        env_file = ".env"
-        case_sensitive = True
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        case_sensitive = True,
+        extra="ignore",  # 未定義の環境変数を無視
+    )
 
 
 settings = Settings()
